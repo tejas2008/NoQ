699460e9fb0598ae4e606c5f5fa8c3d19b5ad107
tejas2008 <tejas.khanolkar@somaiya.edu>
2020-05-21 11:33:05 +0530

    docker-compose added

--- root/app.py
+++ root/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 app.secret_key = 'secret'
 
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'hackathon'
